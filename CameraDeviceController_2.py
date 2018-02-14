73e70ec1f93ce6c8059a4c505215edb18a731dc7
Filip Lindau <filip.lindau@maxlab.lu.se>
2018-02-14 15:35:31 +0100

    modified:   CameraDeviceController_2.py Added comments

--- CameraDeviceController_2.py
+++ CameraDeviceController_2.py
@@ -25,6 +25,18 @@ root.setLevel(logging.DEBUG)
 
 
 class CameraDeviceController(object):
+    """
+    Class for handling states in a camera device. A command list contains commands that are issued and
+    awaiting result.
+
+    Will issue commands setting up an initial state upon entering INIT state. Will also setup a set of
+    polled attributes, such as image.
+
+    Command results are processed in device_command_cb, where recurrend (polled) commands are re-issued.
+
+    If a state change is detected camera_state_cb is called and the internal state is set accordingly.
+    External classes, such as a DeviceServer, can subscribe to state changes, whereby a stored method is called.
+    """
     def __init__(self, device_name, parameter_dict=None):
         self.device_name = device_name
         self.device = None
@@ -79,6 +91,12 @@ class CameraDeviceController(object):
         root.debug("===========================================================")
 
     def unknown_handler(self, prev_state):
+        """
+        Handler for UNKNOWN state: connecting to camera (first disconnecting).
+        Connection is verified by sending read state command.
+        :param prev_state:
+        :return:
+        """
         root.debug("Entering unknown_handler")
         connection_timeout = 1.0
 
@@ -99,6 +117,12 @@ class CameraDeviceController(object):
                     self.wakeup_condition_var.wait(connection_timeout)
 
     def init_handler(self, prev_state):
+        """
+        Handler for INIT state: setting up initial conditions by stopping camera, then issuing
+        commands in a parameter_dict. After that the camera is started again.
+
+        Setting up polled attributes in the command_list
+        """
         root.debug("Entering init_handler")
         timeout = 7.0
 
@@ -153,6 +177,16 @@ class CameraDeviceController(object):
             self.command_list = []
 
     def on_handler(self, prev_state):
+        """
+        Handler for ON, RUNNNING, STANDBY, and ALARM states.
+        All actual processing is done in the device_command_cb callback.
+
+        Additional commands can be set by external class using write_attribute, read_attribute, and
+        exec_command methods.
+
+        :param prev_state:
+        :return:
+        """
         root.debug("Entering on_handler")
         timeout = 0.2
         handled_states = [pt.DevState.ON, pt.DevState.RUNNING, pt.DevState.STANDBY, pt.DevState.ALARM]
@@ -175,6 +209,14 @@ class CameraDeviceController(object):
         pass
 
     def fault_handler(self, prev_state):
+        """
+        Handler for FAULT state.
+
+        Additional fault handling logic should be added here.
+
+        :param prev_state:
+        :return:
+        """
         root.debug("Entering fault_handler")
         timeout = 0.2
         handled_states = [pt.DevState.FAULT]
@@ -183,6 +225,12 @@ class CameraDeviceController(object):
                 self.state_condition_var.wait(timeout)
 
     def connect(self):
+        """
+        Disconnect from camera  by cancelling all outstanding commands.
+        Then create new deviceproxy object.
+
+        :return:
+        """
         self.disconnect()
         root.info("Connecting to {0}".format(self.device_name))
         self.device = None
@@ -195,6 +243,12 @@ class CameraDeviceController(object):
         dev_future.add_done_callback(self.connected_cb)
 
     def connected_cb(self, dev_future):
+        """
+        Callback for device proxy creation. If there is an error, the state is set to UNKNOWN.
+        Normally errors are not detected here.
+        :param dev_future:
+        :return:
+        """
         root.info("Connected callback")
         if dev_future.cancelled() is True:
             root.error("Device future cancelled")
@@ -206,12 +260,12 @@ class CameraDeviceController(object):
             except pt.DevFailed as e:
                 root.error("Device future devfailed {0}".format(str(e)))
                 if e[0].reason == "API_DeviceNotExported":
+                    self.set_status("Device not started")
                     self.set_state(pt.DevState.UNKNOWN)
                     dev = None
-                    self.set_status("Device not started")
                 elif e[0].reason == "DB_DeviceNotDefined":
-                    self.set_state(pt.DevState.UNKNOWN)
                     self.set_status("Device not found in database")
+                    self.set_state(pt.DevState.UNKNOWN)
                     dev = None
                 else:
                     raise
@@ -220,6 +274,12 @@ class CameraDeviceController(object):
             self.state_condition_var.notify_all()
 
     def reset_watchdog(self):
+        """
+        Watchdog to detect a dead camera device.
+        This method must be called periodically to reset a timer.
+
+        :return:
+        """
         if self.watchdog_timer is not None:
             self.watchdog_timer.cancel()
         self.watchdog_timer = threading.Timer(self.watchdog_timeout, self.watchdog_handler)
@@ -230,10 +290,25 @@ class CameraDeviceController(object):
             self.watchdog_timer.cancel()
 
     def watchdog_handler(self):
+        """
+        If the watchdog timer times out this method is invoked.
+        Only sets the state to UNKNOWN for now.
+
+        :return:
+        """
         root.debug("Watchdog timed out. ")
         self.set_state(pt.DevState.UNKNOWN)
 
     def get_attribute(self, attr_name):
+        """
+        Retrieve attribute result.
+
+        Attribute results are stored in a dictionary. If the desired attribute is not in the
+        dictionary, None is returned.
+
+        :param attr_name: Name of attribute to retrieve
+        :return: DeviceAttribute (None if there is no attribute with that name)
+        """
         with self.lock:
             if attr_name in self.attribute_dict:
                 attr = self.attribute_dict[attr_name]
@@ -242,6 +317,16 @@ class CameraDeviceController(object):
         return attr
 
     def write_attribute(self, attr_name, value):
+        """
+        Write attribute value to camera device. Issues three commands:
+        write_attribute
+        delay 0.1 s
+        read_attribute
+
+        :param attr_name: Name of the command
+        :param value: Value to write to the command
+        :return:
+        """
         root.debug("Adding Write_attribute \"{0}\" to {1}".format(attr_name, value))
         with self.lock:
             prev_cmd = dc.DeviceCommand(attr_name, "write", self.device, value)
@@ -259,11 +344,28 @@ class CameraDeviceController(object):
         root.debug("command list length: {0}".format(len(self.command_list)))
 
     def exec_command(self, cmd_name, data=None):
+        """
+        Execute a command on the camera device. No additional commands are sent.
+
+        :param cmd_name: Name of the command (e.g. start)
+        :param data: Optional data used by the command
+        :return:
+        """
         with self.lock:
             cmd = dc.DeviceCommand(cmd_name, "command", self.device, data)
             cmd.start()
 
     def add_polled_attribute(self, attr_name, period, subscriber_method=None):
+        """
+        Add a polled read attribute to the command_list. This is done by setting the recurrent option
+        to True.
+
+        :param attr_name: Name of the attribute
+        :param period: Polling period
+        :param subscriber_method: Optional callback method to call when the the attribute is done. Additionally
+        device_command_cb is added as a subscriber.
+        :return:
+        """
         root.info("Adding attribute {0} with polling period {1} s".format(attr_name, period))
         with self.lock:
             attr = pt.DeviceAttribute()
@@ -276,9 +378,20 @@ class CameraDeviceController(object):
             dev_cmd.start()
 
     def get_state(self):
+        """
+        Return the current internal state.
+
+        :return:
+        """
         return self.state
 
     def set_state(self, new_state):
+        """
+        Set internal state. Call list of subscribers, including the state and the status message.
+
+        :param new_state:
+        :return:
+        """
         root.debug("Setting new state {0}".format(new_state))
         self.state = new_state
         self.set_status("")
@@ -302,6 +415,10 @@ class CameraDeviceController(object):
         self.state_cb_list.append(cb)
 
     def disconnect(self):
+        """
+        Disconnect from device proxy. Cancel watchdog timer and command list. Set state to UNKNOWN.
+        :return:
+        """
         try:
             self.watchdog_timer.cancel()
         except AttributeError:
@@ -314,12 +431,22 @@ class CameraDeviceController(object):
         self.set_state(pt.DevState.UNKNOWN)
 
     def stop_thread(self):
+        """
+        Stop state handler thread by setting stop flag. Disconnect from device proxy.
+        :return:
+        """
         self.stop_state_thread_flag = True
         with self.wakeup_condition_var:
             self.wakeup_condition_var.notify_all()
         self.disconnect()
 
     def setup_parameters(self, parameter_dict):
+        """
+        Setup initial parameter dict. If the state is not UNKNOWN, set state to INIT
+        to ensure that the parameter dict is loaded to the camera.
+        :param parameter_dict:
+        :return:
+        """
         root.debug("Setting up initial parameters for device \"{0}\"".format(self.device_name))
         try:
             for parameter in parameter_dict:
@@ -331,17 +458,34 @@ class CameraDeviceController(object):
             pass
 
     def init_parameters_ready(self, cmd):
+        """
+        Callback method for when the initial parameters are set. Wakes up the condition variable
+        that the init_handler is waiting for.
+        :param cmd:
+        :return:
+        """
         root.debug("init_parameters_ready called")
         with self.wakeup_condition_var:
             self.wakeup_condition_var.notify_all()
 
     def unknown_connect_ready(self, cmd):
+        """
+        Callback method for when connecting to camera and sending read state. Wakes up the condition variable
+        that the unknown_handler is waiting for.
+        :param cmd:
+        :return:
+        """
         root.debug("unknown_connect_ready called")
         if cmd.done is True:
             with self.wakeup_condition_var:
                 self.wakeup_condition_var.notify_all()
 
     def camera_state_cb(self, cmd):
+        """
+        Callback method for read state polling attribute. If the camera is not RUNNING a start command is sent.
+        :param cmd:
+        :return:
+        """
         camera_state = cmd.get_attribute().value
         root.debug("camera_state_cb called, state {0}".format(camera_state))
         if camera_state in [pt.DevState.ON, pt.DevState.STANDBY]:
@@ -360,6 +504,17 @@ class CameraDeviceController(object):
             self.set_state(camera_state)
 
     def device_command_cb(self, cmd_d):
+        """
+        Callback method for attributes in the command_list. If the command had an error this is read out
+        and the internal state is set accordingly.
+
+        Recurrent commands are re-issued.
+
+        Watchdog is reset.
+
+        :param cmd_d:
+        :return:
+        """
         remove_list = list()
         reset_flag = False
         new_state = None
